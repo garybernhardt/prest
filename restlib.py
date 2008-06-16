b86e4a17c61e9bb3c6736297a9206643da42d2aa
Gary Bernhardt <gary.bernhardt@gmail.com>
2008-06-16 16:34:21 -0400

    Added variable substitution to restlib
    --HG--
    branch : trunk

--- restlib.py
+++ restlib.py
@@ -93,22 +93,33 @@ class RestlibUnicode(unicode):
                                   media_type,
                                   self._web_client)
 
-    def request(self, verb, raw, payload):
+    def request(self, verb, href, raw, payload):
         content_type, representation = self._web_client.request(
-            verb, self.href, raw, payload)
+            verb, href, raw, payload)
         return self._build_resource(content_type, representation)
 
-    def get(self, raw=False):
-        return self.request('GET', raw, None)
+    def get(self, *args, **kwargs):
+        # This method uses kwargs for the 'raw' argument instead of a default
+        # argument because it also catches varargs in 'args'.  If it was
+        # declared as get(self, raw=False, *args), and someone called it with
+        # get('some-variable'), then 'some-variable' would come in as 'raw',
+        # not as one of the args.
+
+        # The variable substitution will do nothing if there are no variables
+        # to substitute
+        print self.href
+        print args
+        href = self.href % args
+        return self.request('GET', href, kwargs.get('raw', False), None)
 
     def post(self, payload, raw=False):
-        return self.request('POST', raw, payload)
+        return self.request('POST', self.href, raw, payload)
 
     def put(self, payload, raw=False):
-        return self.request('PUT', raw, payload)
+        return self.request('PUT', self.href, raw, payload)
 
     def delete(self, raw=False):
-        return self.request('DELETE', raw, None)
+        return self.request('DELETE', self.href, raw, None)
 
 
 class Link(RestlibUnicode):
