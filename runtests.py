139013bac78eb15b796b4544de15615035eddc16
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-04-23 17:15:39 -0700

    add runtests.py

---
+++ runtests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+import nose
+
+
+if __name__ == '__main__':
+    nose_args = sys.argv + [r'-s',
+                            r'-m',
+                            r'((?:^|[b_.-])(:?[Tt]est|Describe|When|should|it_))',
+                            r'--with-doctest',
+                            r'--doctest-extension=']
+    nose.run(argv=nose_args)
+
