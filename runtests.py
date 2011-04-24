976f2e5a12942749c23edd388f7336942afb6248
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-04-23 17:28:01 -0700

    allow wider test naming

--- runtests.py
+++ runtests.py
@@ -8,7 +8,7 @@ import nose
 if __name__ == '__main__':
     nose_args = sys.argv + [r'-s',
                             r'-m',
-                            r'((?:^|[b_.-])(:?[Tt]est|Describe|When|should|it_))',
+                            r'((?:^|[b_.-])(:?[Tt]est|[Dd]escribe|When|should|it_))',
                             r'--with-doctest',
                             r'--doctest-extension=']
     nose.run(argv=nose_args)
